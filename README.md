ee272c658b138f4c6f61181629d88ba086882de1
rahulsai1999 <noneuser2183@gmail.com>
2019-10-21 23:14:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Parallel Computing Cluster using Docker
+
+### To build : 
+```
+docker build -t eon01/md5summer .
+```
+
+### Single Threaded :
+```
+time python singlethread.py
+```
+### Multi Threaded :
+```
+time python multithread.py
+```
--- multithread.py
+++ multithread.py
@@ -1,17 +1,33 @@
 from concurrent.futures import ThreadPoolExecutor
 import docker
 import random
+
+
 def fun(message, client):
-    container = client.containers.run("eon01/md5summer", environment=["var=%s" % message], detach=True)
+    container = client.containers.run(
+        "eon01/md5summer", environment=["var=%s" % message], detach=True)
     logs = container.logs()
-    
+
     for line in container.logs(stream=True):
-        print (line.strip())
-client1 = docker.DockerClient(base_url='tcp://54.171.137.205:4243',  tls=False)
-client2 = docker.DockerClient(base_url='tcp://54.229.155.121:4243',  tls=False)
-messages= ["c4ca4238a0b923820dcc509a6f75849b", "c4ca4238a0b923820dcc509a6f758491", "c4ca4238a0b923820dcc509a6f75849c", "c4ca4238a0b923820dcc509a6f75849d", "c4ca4238a0b923820dcc509a6f75849b", "c4ca4238a0b923820dcc509a6f758491", "c4ca4238a0b923820dcc509a6f75849c", "c4ca4238a0b923820dcc509a6f75849d", "c4ca4238a0b923820dcc509a6f75849b", "c4ca4238a0b923820dcc509a6f758491", "c4ca4238a0b923820dcc509a6f75849c", "c4ca4238a0b923820dcc509a6f75849d"]
+        print(line.strip())
+
+
+client1 = docker.from_env()
+messages = [
+    "c4ca4238a0b923820dcc509a6f75849b",
+    "c4ca4238a0b923820dcc509a6f758491",
+    "c4ca4238a0b923820dcc509a6f75849c",
+    "c4ca4238a0b923820dcc509a6f75849d",
+    "c4ca4238a0b923820dcc509a6f75849b",
+    "c4ca4238a0b923820dcc509a6f758491",
+    "c4ca4238a0b923820dcc509a6f75849c",
+    "c4ca4238a0b923820dcc509a6f75849d",
+    "c4ca4238a0b923820dcc509a6f75849b",
+    "c4ca4238a0b923820dcc509a6f758491",
+    "c4ca4238a0b923820dcc509a6f75849c",
+    "c4ca4238a0b923820dcc509a6f75849d"
+]
 pool = ThreadPoolExecutor(50)
-clients = [client1, client2]
- 
+
 for my_message in messages:
-    future = pool.submit(fun, (my_message), random.choice(clients) )
+    future = pool.submit(fun, (my_message), client1)
